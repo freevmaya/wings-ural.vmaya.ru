c870622fae6f3c5cd9f13af1f4b7958853300597
Vadim Frolov <@vmaya>
2026-06-27 21:28:42 +0500

    requlary

--- public_html/index.php
+++ public_html/index.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require __DIR__ . '/../vendor/autoload.php';
